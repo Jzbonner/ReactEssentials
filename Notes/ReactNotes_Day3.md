9bf599835dcdb3515e6c682d6749fa6a9f101ef7
Jzbonner <jarrett.bonner@gmail.com>
2018-04-29 19:37:19 -0400

    changes to React Essentials - Day 2 completed

--- Notes/ReactNotes_Day3.md
+++ Notes/ReactNotes_Day3.md
@@ -1,3 +1,18 @@
 # Day 3 ~ React Essentials: Objects in React and Fetching External Data
 Objects in JavaScript are typically utilized in React Components in React to index stores of information that are defined in key value pairs. They can be referenced specifically in component elements or in the React State of the UI. 
 
+Components can have a life cycle. Components have specific life cycle methods that allow you to manage the state and updated states during implementation of your web application. These include: 
+* `Constructor` which allows you to establish the initial state of your component 
+* `ComponentDidUpdate()`, `ComponentDidMount()`, `console.log('--render--')`, can allow developers to monitor the state of the application 
+* `ComponentWillUnmount()` is used explicitly for monitoring the fetching of external data 
+* Additionally there also is `static getDerivedStateFromProps()` which in some use cases will allow you to get the props from the parent component and set those props on the local state of your component 
+
+Typically when fetching external data you want to make your API request by using the `componentDidMount()` method. It helps to streamline asynchronous rendering. This syntax/implementation is similar to putting JSX in your render component. All of it is to keep elements separated appropriately and easily comparable for your State and UI. 
+
+You want to use a Class Component when dealing with local state. If you have state inside of your component than you need to use a Class. Basically if you have a life cycle event on the component then you need to use a class as opposed to a state less functional component. 
+
+The use of the ternary operator in React is essentially Conditional Rendering. You can find additional information about React and conditional rendering from the [docs](https://reactjs.org/docs/conditional-rendering.html), however for a more in depth look at conditional rendering in React with ternary and logical AND operators please refer to this writeup on [@Medium](https://medium.freecodecamp.org/conditional-rendering-in-react-using-ternaries-and-logical-and-7807f53b6935)  
+
+> In React, you can create distinct components that encapsulate behavior you need. Then, you can render only some of them, depending on the state of your application. You can imagine the ternary operator which will - sort of - implicitly return a value based on a condition
+
+For full functionality of a Friends Tracker Web Application with input functionality and a Loading screen please refer to the project files for Day 3 located in the `/PreviousExampleFiles`. 
--- ReactExamples/main.js
+++ ReactExamples/main.js
@@ -1,125 +1,65 @@
-function ActiveFriends (props) {
-  return (
-    <div>
-      <h2>Active Friends</h2>
-      <ul>
-        {props.list.map((friend) => (
-          <li key={friend.name}>
-            <span>{friend.name}</span>
-            <button onClick={() => props.onRemoveFriend(friend.name)}>Remove</button>
-            <button onClick={() => props.onToggleFriend(friend.name)}>Deactivate</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+/* API functionality for predetermined data */ 
+
+window.API = {
+  fetchPopularRepos(language) {
+    // "language" can be "javascript", "ruby", "python", or "all"
+    const encodedURI = encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
+    return fetch(encodedURI)
+      .then((data) => data.json())
+      .then((repos) => repos.items)
+      .catch((error) => {
+        console.warn(error)
+        return null
+      });
+  }
 }
-function InactiveFriends (props) {
-  return (
-    <div>
-      <h2>Inactive Friends</h2>
-      <ul>
-        {props.list.map((friend) => (
-          <li key={friend.name}>
-            <span>{friend.name}</span>
-            <button onClick={() => props.onToggleFriend(friend.name)}>Activate</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
-class App extends React.Component {
+
+/* Class information for the React Loading Component  */
+
+class Loading extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+
     this.state = {
-      friends: [
-        {
-          name: 'Jordyn',
-          active: true,
-        },
-        {
-          name: 'Mikenzi',
-          active: true,
-        },
-        {
-          name: 'Jake',
-          active: false
-        }
-      ],
-      input: '',
-    }
-    this.handleRemoveFriend = this.handleRemoveFriend.bind(this)
-    this.updateInput = this.updateInput.bind(this)
-    this.handleAddFriend = this.handleAddFriend.bind(this)
-    this.handleToggleFriend = this.handleToggleFriend.bind(this)
+      text: 'Loading'
+    };
   }
-  handleAddFriend() {
-    this.setState((currentState) => {
-      return {
-        friends: currentState.friends.concat([{
-          name: this.state.input,
-          active: true
-        }]),
-        input: ''
-      }
-    })
+
+  componentDidMount() {
+    const stopper = this.state.text + '...';
+    this.interval = window.setInterval(() => {
+      this.state.text === stopper
+        ? this.setState(() => ({ text: 'Loading' }))
+        : this.setState((prevState) => ({ text: prevState.text + '.' }))
+    }, 300)
   }
-  handleRemoveFriend(name) {
-    this.setState((currentState) => {
-      return {
-        friends: currentState.friends.filter((friend) => friend.name !== name)
-      }
-    })
+
+  componentWillUnmount() {
+    window.clearInterval(this.interval);
   }
-  handleToggleFriend(name) {
-    this.setState((currentState) => {
-      const friend = currentState.friends.find((friend) => friend.name === name)
-      return {
-        friends: currentState.friends.filter((friend) => friend.name !== name)
-          .concat([{
-            name,
-            active: !friend.active
-          }])
-      }
-    })
+
+  render() {
+    return (
+      <h1>
+        {this.state.text}
+      </h1>
+    )
   }
-  updateInput(e) {
-    const value = e.target.value
-    this.setState({
-      input: value
-    })
-  }
+}
+
+/* The Class that contains the embodiment of our Application */
+
+class App extends React.Component {
   render() {
     return (
       <div>
-        <input
-          type='text'
-          placeholder='new friend'
-          value={this.state.input}
-          onChange={this.updateInput}
-        />
-        <button onClick={this.handleAddFriend}>
-          Submit
-        </button>
-        <div>
-          <button onClick={() => this.setState({
-            friends: []
-          })}>Clear All</button>
-        </div>
-        <ActiveFriends
-          list={this.state.friends.filter((friend) => friend.active === true)}
-          onRemoveFriend={this.handleRemoveFriend}
-          onToggleFriend={this.handleToggleFriend}
-        />
-        <InactiveFriends
-          list={this.state.friends.filter((friend) => friend.active === false)}
-          onToggleFriend={this.handleToggleFriend}
-        />
+        
       </div>
     )
   }
 }
+
+/* ReactDom Render of the above application */
 
 ReactDOM.render(
   <App />,
